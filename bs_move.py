b6c61e90081aa44d1eae213ba6dae055e7b83012
zvERIK-py <zverik200025@gmail.com>
2026-03-22 10:30:03 +0300

    итоговая работа по условному оператору

---
+++ bs_move.py
@@ -0,0 +1,10 @@
+ # ход слона в шахматах
+a1, a2, b1, b2 = int(input()), int(input()), int(input()), int(input())
+if a1 + a2 == b1 + b2 or a1 - a2 == b1 - b2:
+    print("YES")
+else:
+    print("NO")
+
+
+
+
---
+++ rome_numbers.py
@@ -0,0 +1,33 @@
+ # Напишите программу, которая считывает целое число и выводит соответствующую ему римскую цифру.
+ #  Если число находится вне диапазона [1;10], то программа должна вывести текст «ошибка» (без кавычек).
+
+
+
+num = int(input())
+if 1 <= num <= 10:
+    if num == 1:
+        print("I")
+    elif num == 2:
+        print("II")
+    elif num == 3:
+        print("III")
+    elif num == 4:
+        print("IV")
+    elif num == 5:
+        print("V")
+    elif num == 6:
+        print("VI")
+    elif num == 7:
+        print("VII")
+    elif num == 8:
+        print("VIII")
+    elif num == 9:
+        print("IX")
+    elif num == 10:
+        print("X")
+else :
+    print("ошибка")
+
+
+
+
